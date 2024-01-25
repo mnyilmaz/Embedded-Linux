cc560b8f4cb01b8c4aa92aeffe10cc4813f50a70
Merve Nur YILMAZ <68549106+mnyilmaz@users.noreply.github.com>
2024-01-25 11:31:20 +0300

    Update base.py

--- First_Project/src/base.py
+++ First_Project/src/base.py
@@ -1,7 +1,3 @@
-import serial
-import time
-
-
 # Base class
 class Connection:
     def at_command(self, at_command, explanation):
@@ -11,7 +7,6 @@ class Connection:
             response = modem.read(byte)
             print(f"Received {explanation} response:\n{response.decode()}\n")
             return response
-
     
     # Check if base connection exists
     def check_base(self):
@@ -20,13 +15,11 @@ class Connection:
         self.at_command('AT+CGREG?', 'GPRS registration')
         self.at_command('AT+COPS?', 'Base status')
         
-    
     def define_PDP_context(self):
         apn = input("Enter your APN: ") 
         ip = input("Enter your IP: ") 
         self.at_command(f'AT+CGDCONT=1,"{ip}","{apn}"', 'Define PDP context')
         self.at_command('AT+CGDCONT?', 'Check APN')
-
 
     def reboot_options(self):
         response = self.at_command('AT+QCFG="usbnet"', 'Check USBNET settings')
