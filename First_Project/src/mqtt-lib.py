1f2178f9c29754642462e5b8ce27e0a6fd11d6ac
Merve Nur YILMAZ <68549106+mnyilmaz@users.noreply.github.com>
2024-01-25 11:28:19 +0300

    Update mqtt-lib.py

--- First_Project/src/mqtt-lib.py
+++ First_Project/src/mqtt-lib.py
@@ -1,32 +1,22 @@
 from base import Connection
+import inputs as var
 
 # MQTT
 class MQTT:
     con = Connection()
-
-    def __init__(self, mode, broker, port, subscribe, client, qos, topic, publish, message_length)
-        self.mode = mode
-        self.broker = broker
-        self.port = port
-        self.client = client
-        self.qos = qos
-        self.subscribe = subscribe
-        self.topic = topic
-        self.publish = publish
-        self.message_lenght = message_length
         
     def connect(self):
-        self.con.at_command(f'AT+QMTCFG="{self.mode}",0,0,1', 'Set mode')
-        self.con.at_command(f'AT+QMTOPEN=0,"{self.broker}","{self.port}"', 'Broker connection')
-        self.con.at_command(f'AT+QMTCONN=0,"{self.client}"', 'Connect to the client') 
+        self.con.at_command(f'AT+QMTCFG="{var.mode}",0,0,1', 'Set mode')
+        self.con.at_command(f'AT+QMTOPEN=0,"{var.broker}","{var.port}"', 'Broker connection')
+        self.con.at_command(f'AT+QMTCONN=0,"{var.client}"', 'Connect to the client') 
     
     def subscribe(self):
-        self.con.at_command(f'AT+QMTSUB=0,"{self.qos}","{self.subscribe}","{self.topic}"', 'Subscribe a topic')
-        self.con.at_command(f'AT+QMTUNS=0,"{self.topic}","{self.subscribe}"', 'Unsubscribe a topic')
+        self.con.at_command(f'AT+QMTSUB=0,"{var.qos}","{var.subscribe}","{var.topic}"', 'Subscribe a topic')
+        self.con.at_command(f'AT+QMTUNS=0,"{var.topic}","{var.subscribe}"', 'Unsubscribe a topic')
         self.con.at_command('AT+QMTCLOSE=0', 'Close Net')
         
     def publish(self):
-        self.con.at_command(f'AT+QMTPUBEX=0,0,0,0,"{self.publish}","{self.message_length}"', 'Publish message to a topic')
+        self.con.at_command(f'AT+QMTPUBEX=0,0,0,0,"{var.publish}","{var.message_length}"', 'Publish message to a topic')
         message = input("Enter your message to the MQTT: ")
         self.con.at_command(message, 'Message')
         self.con.at_command('AT+QMTRECV=0', 'Received')
