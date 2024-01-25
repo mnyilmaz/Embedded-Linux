f1ae29e71f3f589d0c1c825fc42aaa09e7b0a758
Merve Nur YILMAZ <68549106+mnyilmaz@users.noreply.github.com>
2024-01-25 11:13:18 +0300

    Update main.py

--- First_Project/src/main.py
+++ First_Project/src/main.py
@@ -1,64 +1,62 @@
-import serial
+import serial, time, base, http-lib, mqtt-lib 
 
-if __name__ == "__main__":
-    modem = serial.Serial('/dev/ttyUSB3', 115200, timeout=5)
-    byte = 1024
+# Main configurations
+baudrate = 115200
+port = 'dev/ttyUSB3'
+byte = 1024
+apn = 'super'
+modem = serial.Serial(port, baudrate, timeout=5)
+base = base.Connection()
 
-    BASE = {
-        'sim': 'AT+CPIN?',
-        'network': 'AT+CREG?',
-        'gprs': 'AT+CGREG?',
-        'base': 'AT+COPS?',
-        'ue_reboot': 'AT+CFUN=1,1'
-    }
-
-    HTTP_AT = {
-        'querry_PDP': 'AT+QIACT?',
-        'activate': 'AT+QIACT=1',
-        'deactivate': 'AT+QIDEACT=1',
-        'url': 'https://webhook.site/',
-        'check_url': 'AT+QHTTPURL?',
-        'get_request': 'AT+QHTTPGET=80',
-        'read':'AT+QHTTPREAD=80',
-        'post_request': 'AT+QHTTPPOST=10,50',
-        'message': 'hello http',
-        'cancel': 'AT+QHTTPSTOP',
-        'end': 'AT+QICLOSE=1'  
-    }
+# HTTP
+url = 'https://webhook.site/'
+get_size = 80
+read_size = 80
+post_size = 20
+latency = 50
+http = http-lib.HTTP(apn, url, get_size, read_size, post_size, latency)
     
-    MQTT_AT = {
-        'set': 'AT+QMTCFG="recv/mode",0,0,1',
-        'open_net': 'AT+QMTOPEN=0,"broker.hivemq.com",1883', # Open a network for MQTT client id, hostname, port
-        'is_open': 'AT+QMTOPEN?', # Check status 
-        'connect': 'AT+QMTCONN=0,"embedded"', # clientID
-        'sub1': 'AT+QMTSUB=0,1,"topic/example",2', # clientID, messageID, topic
-        'sub2': 'AT+QMTSUB=0,1,"topic/pub",0', # clientID, messageID, topic
-        'publish': 'AT+QMTPUBEX=0,0,0,0,"topic/pub",30', # clientID, messageID, qos
-        'message': 'hello mqtt',
-        'read': 'AT+QMTRECV=0', # clientID
-        'close_net': 'AT+QMTCLOSE=0', # clientID
-        'disconnect': 'AT+QMTDISC=0', # Disconnect a client
-        'unsubscribe': 'AT+QMTUNS=0,1,"topic/example"' # cleintID, messageID, topic      
-    }   
+# MQTT
+mode = "recv/mode"
+broker = "broker.hivemq.com"
+client_port = 1883
+client = "embedded"
+qos = 1
+topic = 1
+subscribe = "topic/sub"
+publish = "topic/pub"
+message_length = 80
+mqtt = mqtt-lib.MQTT(mode, broker, port, subscribe, client, qos, topic, publish, message_length)
 
-    # Base connection
-    connect = Connection()
-    #connect.check_base()
-    #connect.set_APN()
+def control_base(self):
+    base.check_base()
 
-    # HTTP connection
-    #http = HTTP()
-    #http.config()
-    #http.set_PDP()
-    #http.connect()
-    #http.http_get()
-    #http.http_read()
-    #http.http_post()
+def http_get(self):
+    http.set_PDP()
+    http.connect()
+    http.http_get()
+    
+ def http_post(self): 
+    http.set_PDP()
+    http.connect()
+    http.http_get()
 
-    # MQTT connection
-    mqtt = MQTT()
+def mqtt_sub(self):
     mqtt.connect()
-    #mqtt.subscribe()
-    mqtt.publish_message()
+    mqtt.subscribe()
+
+def mqtt_pub(self):
+    mqtt.connect()
+    mqtt.publish()
     
+if __name__ == "__main__":
+    # Check
+    control_base()
+
+    # HTTP
+    http_get()
+
+    # MQTT
+    mqtt_pub()
+
     modem.close()
