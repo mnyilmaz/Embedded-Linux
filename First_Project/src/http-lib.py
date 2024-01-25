54d09ca9e79c4d610b836b93ba3fe6bc9a5769ea
Merve Nur YILMAZ <68549106+mnyilmaz@users.noreply.github.com>
2024-01-25 10:57:28 +0300

    Update http-lib.py

--- First_Project/src/http-lib.py
+++ First_Project/src/http-lib.py
@@ -2,7 +2,15 @@ from base import Connection
 
 class HTTP:
     con = Connection()
-    
+
+    def __init__(self, apn, url, get_size, read_size, post_size, latency):
+        self.apn = apn
+        self.url = url
+        self.get_size = get_size
+        self.read_size = read_size
+        self.post_size = post_size
+        self.latency = latency
+
     # HTTP settings
     def config(self):
         self.con.at_command('AT+QHTTPCFG="contextid",1', 'Context ID Configuration')
@@ -15,52 +23,49 @@ class HTTP:
 
     def QHTTPCFG_settings(self):
         request_header = input("request_header: ")
-        self.con.at_command("AT+QHTTPCFG=\"requestheader\",\"{}\"".format(request_header), 'AT+QHTTPCFG=requestheader')
+        self.con.at_command(f'AT+QHTTPCFG="requestheader","{request_header}"', 'AT+QHTTPCFG=requestheader')
         sslctxID = input("sslctxID: ")
-        self.con.at_command("AT+QHTTPCFG=\"sslctxid\",\"{}\"".format(sslctxID), 'AT+QHTTPCFG=sslctxid')
+        self.con.at_command(f'AT+QHTTPCFG="sslctxID","{sslctxID}"', 'AT+QHTTPCFG=sslctxID')
         content_type = input("content_type: ")
-        self.con.at_command("AT+QHTTPCFG=\"contenttype\",\"{}\"".format(content_type), 'AT+QHTTPCFG=contenttype')
+        self.con.at_command(f'AT+QHTTPCFG="contenttype","{content_type}"', 'AT+QHTTPCFG=contenttype')
         auto_outrsp = input("auto_outrsp: ")
-        self.con.at_command("AT+QHTTPCFG=\"rspout/auto\",\"{}\"".format(auto_outrsp), 'AT+QHTTPCFG=rspout/auto')
+        self.con.at_command(f'AT+QHTTPCFG="rspout/auto","{auto_outrsp}"', 'AT+QHTTPCFG=rspout/auto')
         closedind = input("closedind: ")
-        self.con.at_command("AT+QHTTPCFG=\"closed/ind\",\"{}\"".format(closedind), 'AT+QHTTPCFG=closed/ind')
+        self.con.at_command(f'AT+QHTTPCFG="closed/ind","{closedind}"', 'AT+QHTTPCFG=closed/ind')
         window_size = input("window_size: ")
-        self.con.at_command("AT+QHTTPCFG=\"windowsize\",\"{}\"".format(window_size), 'AT+QHTTPCFG=windowsize')
+        self.con.at_command(f'AT+QHTTPCFG="windowsize","{window_size}"', 'AT+QHTTPCFG=windowsize')
         close_wait_time = input("close_wait_time: ")
-        self.con.at_command("AT+QHTTPCFG=\"closewaittime\",\"{}\"".format(close_wait_time), 'AT+QHTTPCFG=closewaittime')
+        self.con.at_command(f'AT+QHTTPCFG="closewaittime","{close_wait_time}"', 'AT+QHTTPCFG=closewaittime')
         username = input("username: ")
         password = input("password: ")
-        self.con.at_command("AT+QHTTPCFG=\"auth\",\"%s\":\"%s\"".format(username, password), 'AT+QHTTPCFG=auth')
-        custom_value = input("custom_value: ")
-        self.con.at_command("AT+QHTTPCFG=\"custom_header\",\"{}\"".format(custom_value), 'AT+QHTTPCFG=custom_header')
+        self.con.at_command(f'AT+QHTTPCFG="auth","{username}":"{password}"', 'AT+QHTTPCFG=auth')
+        custom_header = input("custom_header: ")
+        self.con.at_command(f'AT+QHTTPCFG="custom_header","{custom_header}"', 'AT+QHTTPCFG=custom_header')
         
     def set_PDP(self):
-        apn = input("Enter APN: ")
-        self.con.at_command(f'AT+QICSGP=1,1,"{apn}","","",1', 'Set APN')
-        self.con.at_command(HTTP_AT['querry_PDP'], 'Querry PDP Context')
-        self.con.at_command(HTTP_AT["activate"], 'Activate PDP Context')
+        self.con.at_command(f'AT+QICSGP=1,1,"{self.apn}","","",1', 'Set APN')
+        self.con.at_command('AT+QIACT?', 'Querry PDP Context')
+        self.con.at_command('AT+QIACT=1', 'Activate PDP Context')
+        self.con.at_command('AT+QIDEACT=1', 'Deactivate PDP Context')
 
     def connect(self):
-        self.con.at_command(f'AT+QHTTPURL={len(HTTP_AT["url"])},80', 'Set URL')
+        self.con.at_command(f'AT+QHTTPURL={len(self.url)},80', 'Set URL pre-settings')
         time.sleep(10)
-        self.con.at_command(HTTP_AT['url'], 'URL')
+        self.con.at_command(self.url, 'Set URL')
         time.sleep(5)
-        self.con.at_command(HTTP_AT['check_url'], 'Check URL status')
+        self.con.at_command('AT+QHTTPURL?', 'Check URL status')
         
     def http_get(self):
         time.sleep(20)
-        self.con.at_command(HTTP_AT['get_request'], 'HTTP GET Request')
-        
-    def http_read(self):
+        self.con.at_command(f'AT+QHTTPGET="{self.get_size}"', 'HTTP GET Request')
         time.sleep(20)
-        self.con.at_command(HTTP_AT['read'], 'Read HTTP Response')
+        self.con.at_command(f'AT+QHTTPREAD="{self.read_size}"', 'Read HTTP Response')
         
     def http_post(self):
-        self.con.at_command(HTTP_AT['post_request'], 'HTTP POST Request')
-        self.con.at_command(HTTP_AT['message'], 'Message')
-
-    def http_stop(self):
-        self.con.at_command(HTTP_AT['cancel'], 'Cancel')
+        self.con.at_command(f'AT+QHTTPPOST="{self.post_size}","{self.latency}"', 'HTTP POST Request')
+        message = input("Enter your message to the HTTP: ")
+        self.con.at_command(message, 'Message')
         
-    def close_connection(self):
-        self.con.at_command(HTTP_AT['end'], 'End the connection')
+    def destroyer_of_http(self):
+        self.con.at_command('AT+QHTTPSTOP', 'Cancel')
+        self.con.at_command('AT+QICLOSE=1', 'End the connection')
