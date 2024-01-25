31c6d59c85d1b4c92d3dfa0e002a9135c91d71cd
Merve Nur YILMAZ <68549106+mnyilmaz@users.noreply.github.com>
2024-01-25 11:33:58 +0300

    Update main.py

--- First_Project/src/main.py
+++ First_Project/src/main.py
@@ -1,36 +1,18 @@
 import serial, time, base, http-lib, mqtt-lib 
 
-# Main configurations
-baudrate = 115200
-port = 'dev/ttyUSB3'
-byte = 1024
-apn = 'super'
-modem = serial.Serial(port, baudrate, timeout=5)
+# Base
 base = base.Connection()
 
 # HTTP
-url = 'https://webhook.site/'
-get_size = 80
-read_size = 80
-post_size = 20
-latency = 50
-http = http-lib.HTTP(apn, url, get_size, read_size, post_size, latency)
+http = http-lib.HTTP()
     
 # MQTT
-mode = "recv/mode"
-broker = "broker.hivemq.com"
-client_port = 1883
-client = "embedded"
-qos = 1
-topic = 1
-subscribe = "topic/sub"
-publish = "topic/pub"
-message_length = 80
-mqtt = mqtt-lib.MQTT(mode, broker, port, subscribe, client, qos, topic, publish, message_length)
+mqtt = mqtt-lib.MQTT()
 
 def control_base(self):
     base.check_base()
 
+# HTTP Implementations
 def http_get(self):
     http.set_PDP()
     http.connect()
@@ -41,6 +23,7 @@ def http_get(self):
     http.connect()
     http.http_post()
 
+# MQTT Implementations
 def mqtt_sub(self):
     mqtt.connect()
     mqtt.subscribe()
